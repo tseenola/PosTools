4fb4b4b6c6bd95605cf4e801673d40421fcb6eb9
helloJun <641380205@qq.com>
2020-08-24 10:53:17 +0800

    增加磁道sm4算法

--- app/src/androidTest/java/com/tseenola/postools/postools/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tseenola/postools/postools/ExampleInstrumentedTest.java
@@ -3,6 +3,7 @@ package com.tseenola.postools.postools;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 import android.util.Pair;
 
 import com.tseenola.postools.security.des.TripleDes_Key16;
@@ -16,6 +17,8 @@ import com.tseenola.postools.security.utils.ConvertUtils;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Arrays;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -56,5 +59,82 @@ public class ExampleInstrumentedTest {
         Mac_9606 lMac_unionEcb = new Mac_9606();
         Pair<Boolean, EncryResult> lMac = lMac_unionEcb.getMac(Constant.SOFT, ConvertUtils.hexStringToByte(sm4Key),
                 null, ConvertUtils.hexStringToByte(needCallMac), new TripleDes_Key16());
+    }
+
+    /**
+     * 测试磁道加密sm4
+     */
+    @Test
+    public   void testSm4Track2() throws Exception {
+        String track2 = "123456789012345678901234567890";//小于32偶数
+        encryDataSM4_2(track2,2);
+        getTrack2EncryData(track2);
+        Log.d( " DEBUG2", "....................................: "   );
+        String track3 = "12345678901234567890123456789";//小于32奇数
+        encryDataSM4_2(track3,2);
+        getTrack2EncryData(track3);
+        Log.d( " DEBUG2", "....................................: "   );
+        String track4 = "12345678901234567890123456789011";//等于32
+        encryDataSM4_2(track4,2);
+        getTrack2EncryData(track4);
+        Log.d( " DEBUG2", "....................................: "   );
+        String track5 = "123456789012345678901234567890112";//大于32奇数
+        encryDataSM4_2(track5,2);
+        getTrack2EncryData(track5);
+        Log.d( " DEBUG2", "....................................: "   );
+        String track6 = "1234567890123456789012345678901123";//大于32偶数
+        encryDataSM4_2(track6,2);
+        getTrack2EncryData(track6);
+        Log.d( " DEBUG2", "....................................: "   );
+    }
+
+
+    public   String getTrack2EncryData(String data) {
+        String track2 = data;
+        if (data.length()%2!=0) {
+            track2 += "F";
+        }
+        track2 = ConvertUtils.fillContentBy(ConvertUtils.Dir.right,"F",track2,34);
+        Log.d(" DEBUG2", "getTrack2EncryData 预处理结果: " + track2);
+        String ss = track2.substring(track2.length()-((16+1)*2),track2.length()-( 1 *2));
+        Log.d( " DEBUG2", "getTrack2EncryData 被加密数据: " + ss);
+        return ss;
+    }
+
+    public   void encryDataSM4_2(String data,int pTrackKeyIndex) throws Exception {
+        boolean lenthMore32 = false;
+        byte[] byteData;
+        byte[] inputData;
+        byte[] outputDate;
+        if (data.length() < 32) {
+            //byteData = Funs.StrToHexByte(formatData32(data));
+            byteData = ConvertUtils.hexStringToByte(formatData32(data));
+        } else if (data.length() == 32) {
+            //byteData = Funs.StrToHexByte(data + "FF");
+            byteData = ConvertUtils.hexStringToByte(data + "FF");
+        } else if (data.length() % 2 != 0) {
+            //byteData = Funs.StrToHexByte(data + "F");
+            byteData = ConvertUtils.hexStringToByte(data + "F");
+            lenthMore32 = true;
+        } else {
+            //byteData = Funs.StrToHexByte(data);
+            byteData = ConvertUtils.hexStringToByte(data);
+        }
+        Log.d(" DEBUG2", "encryDataSM4_2 预处理结果: " + ConvertUtils.bytesToHexString(byteData));
+        inputData = new byte[16];
+        Arrays.fill(inputData, (byte) 0xFF);
+        System.arraycopy(byteData, byteData.length - 17, inputData,0, 16);
+        Log.d( " DEBUG2", "encryDataSM4_2 被加密数据: " + ConvertUtils.bytesToHexString(inputData));
+    }
+
+    private   String formatData32(String card){
+        StringBuffer sb = new StringBuffer();
+        int len = card.length();
+        for (int i = 0;i < 32-len;i++){
+            sb.append("F");
+        }
+        sb.append("FF");
+        String ssdfs = card+sb.toString();
+        return card+sb.toString();
     }
 }
---
+++ security/src/main/java/com/tseenola/postools/security/pos/track/Track_Union_Sm4.java
@@ -0,0 +1,59 @@
+package com.tseenola.postools.security.pos.track;
+
+import android.text.TextUtils;
+import android.util.Pair;
+
+import com.tseenola.postools.security.intface.ISecurity;
+import com.tseenola.postools.security.model.EncryResult;
+import com.tseenola.postools.security.utils.Constant;
+import com.tseenola.postools.security.utils.ConvertUtils;
+
+/**
+ * Created by lijun on 2020/8/17.
+ *  描述：银联标准加密
+ *  2017年7月17日 by lijun for：
+ *  算法描述
+ *  1  对于二磁道或三磁道缺失的情况，终端应上送16字节全F
+ *  2  二磁道数据（35域）从结束标志“？”向左第2个字节开始，再向左取16个字节作为参与加密的二磁道中发卡方信息，记为TDB2。
+ *  3  类似二磁道数据源构造方法，三磁道数据（36域，如果存在）磁道信息块构造方法如下：
+ *         三磁道数据（36域）从结束标志“？”向左第2个字节开始，再向左取16个字节作为参与加密的三磁道中发卡方信息（若不足右补足F），记为TDB3。
+ *  4  采用双倍长密钥TDK分别对TDB2，TDB3进行加密，将密文输出后按照对应位置替换原先的明文数据。
+ */
+public class Track_Union_Sm4 implements ITrackCaculator{
+
+    private String TAG = this.getClass().getSimpleName();
+
+    @Override
+    public Pair<Boolean, EncryResult> getEncryedTrack(int pSecurityBy, byte[] pEncDecKey, Object pSecurityHardParam, String pExplainTrack, int pTrackType, ISecurity pSecurity) {
+        try{
+            //1  对于二磁道或三磁道缺失的情况，终端应上送8字节全F
+            String src = pExplainTrack;
+            if (TextUtils.isEmpty(src)) {
+                return Pair.create(true,new EncryResult(ConvertUtils.hexStringToByte("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")));
+            }
+            src = src.replace('=','D');
+            src = ConvertUtils.fillContentBy(ConvertUtils.Dir.right,"F",src,34);
+            //2,3
+            String encryTemp = src.substring(src.length()-((16+1)*2),src.length()-( 1 *2));
+            //4  采用双倍长密钥TDK分别对TDB2，TDB3进行加密，将密文输出后按照对应位置替换原先的明文数据。
+            byte[] encryOut = new byte[16];
+            Pair<Boolean, EncryResult> lEncryResultPair = null;
+            if (pSecurityBy == Constant.SOFT) {
+                lEncryResultPair = pSecurity.encryDataSoft(ConvertUtils.hexStringToByte(encryTemp),pEncDecKey);
+            }else if (pSecurityBy == Constant.HARD){
+                lEncryResultPair = pSecurity.encryDataHard(ConvertUtils.hexStringToByte(encryTemp),pSecurityHardParam);
+            }else {
+                return Pair.create(false,new EncryResult("无效的参数【加密方式】"));
+            }
+            if (!lEncryResultPair.first) {
+                return lEncryResultPair;
+            }
+            encryOut = lEncryResultPair.second.getEncryDecryResult();
+            String trackEnryData = src.replaceFirst(encryTemp,ConvertUtils.bytesToHexString(encryOut));
+            return Pair.create(true,new EncryResult(ConvertUtils.hexStringToByte(trackEnryData)));
+        }catch(Exception pE){
+            pE.printStackTrace();
+            return Pair.create(false,new EncryResult(pE.getMessage()));
+        }
+    }
+}

64749ce6155e09baaf1ef9ecb0ba46f5c4333c48
helloJun <641380205@qq.com>
2020-12-04 18:21:25 +0800

    增加注释

--- security/src/main/java/com/tseenola/postools/security/pos/mac/algorithm/international/Mac_UnionEcb.java
+++ security/src/main/java/com/tseenola/postools/security/pos/mac/algorithm/international/Mac_UnionEcb.java
@@ -31,7 +31,8 @@ public class Mac_UnionEcb<T> implements IMacCaculator<T> {
      * @param pSecurityBy      加密方式：1、软加密：传入密钥和数据，直接加密，
      *                         或者2、硬加密：传入参数给底层芯片，用芯片加密 {@link Constant.SecurityBy}
      * @param pEncDecKey         软加密传入的Key
-     * @param pSecurityHardParam  硬加密参数，如果是软加密(pSecurityBy值为 SOFT )时 这个参数传 null 硬加密需要传入的参数（主密钥，密钥索引，密钥类型，加密方式des,sm4等等.....）
+     * @param pSecurityHardParam  硬加密参数，如果是软加密(pSecurityBy值为 SOFT )时 这个参数传 null
+     *                            硬加密需要传入的参数（主密钥，密钥索引，密钥类型，加密方式des,sm4等等.....）
      * @param pNeedCallMacDatas  待计算mac的数据
      * @param pSecurity         加密算法，SM4,3DES 等等
      *                          {@link com.tseenola.postools.security.sm.Sm4}
